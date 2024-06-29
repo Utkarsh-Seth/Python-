15491f34320f5966314f399b39261d7eb081c9bf
Utkarsh Seth <mygamingchannel9140@gmail.com>
2024-06-29 23:29:32 +0530

    Update problem4.py

--- Chapter - 1 PS /problem4.py
+++ Chapter - 1 PS /problem4.py
@@ -1,19 +1,10 @@
 import os
 
-# Specify the directory you want to list
-directory_path = '.'  # '.' refers to the current directory
+# Select the directory whose content you want to list 
+directory_path = '/'
 
-try:
-    # Get the list of all files and directories
-    contents = os.listdir(directory_path)
-    
-    print(f"Contents of '{directory_path}':")
-    for item in contents:
-        print(item)
+# Use the os module to list the directory content 
+contents = os.listdir(directory_path)
 
-except FileNotFoundError:
-    print(f"The directory '{directory_path}' does not exist.")
-except PermissionError:
-    print(f"Permission denied to access '{directory_path}'.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# Print the contents of the directory 
+print(contents)
